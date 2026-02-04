44bdc22f2fc549378ce521776f423a988fdabb68
Barakah Chaser <shobolamustapha@yahoo.com>
2026-02-04 13:43:01 -0600

    Add .eslintignore and revert build command

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // IGNORE all TypeScript errors so we can deploy fast
     ignoreBuildErrors: true,
   },
-  // Do NOT put an eslint section here, it causes crashes in Next.js 16
 };
 
 export default nextConfig;
